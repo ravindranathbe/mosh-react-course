9913ad056366b3bcdcdae7bf94f2870a8160e9f7
Ravi <testravi1986@gmail.com>
2026-03-29 11:51:46 +0530

    feat: replace Like component with drink price state and increment button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Like from "./components/Like";
+import { useState } from "react";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Water",
+    price: 0,
+  });
+
   return (
-    <div>
-      <Like onClick={() => console.log("Liked")} />
-    </div>
+    <>
+      <h1>Title: {drink.title}</h1>
+      <p>Price: ${drink.price}</p>
+      <button
+        className="btn btn-primary"
+        onClick={() => setDrink({ ...drink, price: drink.price + 1 })}
+      >
+        Increase price
+      </button>
+    </>
   );
 }
 
